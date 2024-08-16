5f4311d5c9188d08b5b6cc7318e59f6aed0a220e
= <=>
2024-08-16 16:31:45 -0400

    add lactate

--- Rscripts/models/model0LR.R
+++ Rscripts/models/model0LR.R
@@ -7,22 +7,22 @@ mod <- function(Time, State, Pars){
     Sd <- (1-(R+G)/((R+G)+Gmin))*tanh(Time*kp)
     Sp <- 1-Sd
     
-    
-    #dL <- -2*dG ## lactate
+    dG <- -v*N*G/(G+Gstar)
+    dL <- -2*dG ## lactate
     dR <- -vr*N*R/(R+Rstar) ## glucose
     
-    dN <- kp*N*(1-N/theta)*Sp-kd*N*Sd-kbys*N*D/(D+N)
-    dD <- kd*N*Sd+kbys*N*D/(D+N)
-    dG <- -v*N*G/(G+Gstar)
-    return(list(c(dN,dD,dG,dR)))
+    dN <- kp*N*(1-N/theta)*Sp-kd*N*Sd-kbys*N*D/(D+N)-kdl*L/(L+L50)
+    dD <- kd*N*Sd+kbys*N*D/(D+N)+kdl*L/(L+L50)
+    
+    return(list(c(dN,dD,dG,dR,dL)))
   })
 }
 
 ## wrapper to run ODE model
 run_mod <- function(p0,times,y0){
-  base_y0 <- c(N=1000,D=0,G=5,R=10)
+  base_y0 <- c(N=1000,D=0,G=5,R=10,L=0)
   base_pars<-c(kp=0.1,kd=0.1,kbys=0.1,theta=15000,v=0.00001,Gmin=0.1,Gstar=0.1,
-               vr=0.00001,Rstar=0.1)
+               vr=0.00001,Rstar=0.1,kdl=0.1,L50=25)
   base_pars[names(p0)] <- p0
   base_y0[names(y0)] <- y0
   ode(base_y0, times, mod, base_pars)
@@ -31,9 +31,9 @@ run_mod <- function(p0,times,y0){
 ##info required for running or fitting model
 
 model_info <- function(){
-  parnames <- c("theta", "kp", "kd", "kbys", "v"  , "Gmin", "Gstar","vr" ,"Rstar")
-  lower <-    c(6000   , 0.01, 0.01, 0.001 , 1e-8 , 0.01  , 0.01   ,1e-8 ,0.01)
-  upper <-    c(50000  , 1   , 5   , 1     , 0.001, 20    , 20     ,0.001,20)
+  parnames <- c("theta", "kp", "kd", "kbys", "v"  , "Gmin", "Gstar","vr" ,"Rstar","kdl","L50")
+  lower <-    c(6000   , 0.01, 0.01, 0.001 , 1e-8 , 0.01  , 0.01   ,1e-8 ,0.01   ,0.001,5)
+  upper <-    c(50000  , 1   , 5   , 1     , 0.001, 20    , 20     ,0.001,20     ,1    ,50)
   list(parnames=parnames,upper=upper,lower=lower)
 }
 
